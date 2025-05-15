9ef3df2219af90714af8db1a4649afda0673cc37
N48i1 <n48i1elhilali@gmail.com>
2025-05-15 20:13:30 +0100

    Fixed package.json and vite.config.ts for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/N48I1/", 
   plugins: [react()],
   resolve: {
     alias: {
@@ -10,4 +11,3 @@ export default defineConfig({
     },
   },
 })
-
